3af03e9e34b3230327850662f1e066545cd1ada4
florestan lefevre <florestanlefevre@gmail.com>
2018-01-27 15:45:41 -0800

    Add play/pause skip buttons volume and playbackrate controls

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,14 +1,43 @@
 // Get elements
 const player = document.querySelector(`.player`);
 const video = player.querySelector( `.viewer`);
+const toggle = player.querySelector(`.toggle`);
 const progress = player.querySelector(`.progress`);
 const progressFilled = player.querySelector(`.progress__filled`);
-const toggle = player.querySelector(`.toggle`);
 const ranges = player.querySelectorAll(`.player__slider`);
-const skipButtons = player.querySelectorAll(`.player__button`);
+const skipButtons = player.querySelectorAll(`[data-skip]`);
+
+// Create functions()
+
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function toggleButton() {
+  const icon = video.paused ? '||' : '►';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+video[this.name] = this.value;
+}
 
 // Hook up addEventListeners
 
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
 
+video.addEventListener('play', toggleButton);
+video.addEventListener('pause', toggleButton);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 
