a9d286acc3e262f345c1bb0fe3f0e0a29fd29052
florestan lefevre <florestanlefevre@gmail.com>
2018-01-30 16:04:20 -0800

    Custom effects on webcam

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -22,6 +22,15 @@ function paintToCanvas() {
 
   setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height) // (0, 0) = top left corner / (width, height) => dimension of image
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0 , width, height); // get the million pixels of the image in an array as RGBA. Array is too big to use map
+    // mess with pixels
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
+    ctx.globalAlpha = 0.1; // ghost effect !!
+    // put pixels back
+    ctx.putImageData(pixels, 0, 0)
   }, 16);
 }
 
@@ -38,6 +47,53 @@ function takePhoto() {
   // link.textContent = 'Download photo';
   link.innerHTML = `<img src="${data}" alt="webcam snap">` // create an html instead of a link like previously
   strip.insertBefore(link, strip.firstChild); // equivalent of prepend
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i] = pixels.data[i] * 2;     // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;// green
+    pixels.data[i + 2] = pixels.data[i + 2] / 2; // blue
+    // pixels[i + 4] we do not need alpha
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i];     // red
+    pixels.data[i + 100] = pixels.data[i + 1];// green
+    pixels.data[i - 150] = pixels.data[i + 2]; // blue
+    // pixels[i + 4] we do not need alpha
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i += 4) {
+    red = pixels.data[i + 0];   // red
+    green = pixels.data[i + 1]; // green
+    blue = pixels.data[i + 2];  // blue
+    alpha = pixels.data[i + 3]; // alpha
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // If all are between min-max range, take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 video.addEventListener('canplay', paintToCanvas);
